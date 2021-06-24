e371ab8a70c9d378e199d5f0f6158492d50f5e8d
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-06-24 10:13:34 +1000

    return date

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
 import format from "https://deno.land/x/date_fns@v2.22.1/format/index.js";
 addEventListener("fetch", (event: any) => {
-	const d = format(new Date(), "yyyy-MM-dd", null);
-	event.respondWith(new Response(`current Date is ${d}`));
+	event.respondWith(
+		new Response(
+			JSON.stringify({
+				currentDate: format(new Date(), "yyyy-MM-dd", null),
+			}),
+			{
+				headers: {
+					"content-type": "application/json; charset=UTF-8",
+				},
+			}
+		)
+	);
 });
